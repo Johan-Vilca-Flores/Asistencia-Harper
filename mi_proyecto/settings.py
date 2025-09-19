d4c11a9234d4af382a53709dfa6c80fedd11b95f
Johan-Vilca-Flores <144288900+Johan-Vilca-Flores@users.noreply.github.com>
2025-09-18 21:39:22 -0500

    Update settings.py

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -140,8 +140,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # CORS (si frontend está en Netlify)
 CORS_ALLOWED_ORIGINS = [
-    "https://68ccbbb160272854d34ce60e--shimmering-biscochitos-cdba6b.netlify.app"
+    "https://shimmering-biscochitos-cdba6b.netlify.app"
 ]
+
 
 
 
